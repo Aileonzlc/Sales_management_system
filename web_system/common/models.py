4e5d526e4f89641fee12cb08202a7f108999238b
Aileon <767872313@qq.com>
2020-05-13 20:43:48 +0800

    create model medicine;create model order

--- web_system/common/models.py
+++ web_system/common/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Customer(models.Model):
     # 客户名称
@@ -10,5 +11,26 @@ class Customer(models.Model):
 
     # 地址
     address = models.CharField(max_length=200)
-    #  qq
-    QQ = models.CharField(max_length=20, null=True, blank=True, default='NO')
+    # #  qq
+    # QQ = models.CharField(max_length=20, null=True, blank=True, default='NO')
+
+
+class Medicine(models.Model):
+    # 药品名
+    name = models.CharField(max_length=200)
+    # 药品编号
+    sn = models.CharField(max_length=200)
+    # 描述
+    desc = models.CharField(max_length=200)
+
+
+import datetime
+
+
+class Order(models.Model):
+    # 订单名
+    name = models.CharField(max_length=200, null=True, blank=True)
+    # 创建日期
+    create_date = models.DateTimeField(default=datetime.datetime.now)
+    # 客户
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
